92293bdd1475523e1c9ea50684a9ff9fd4b1b3ef
Ranjith <ranjithmailedin@gmail.com>
2024-06-27 23:32:17 +0530

    my commit

---
+++ src/main/java/org/example/Bus.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Bus {
+
+    private String name;
+    Bus(String newName){
+        name = newName;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+
+
+    }
+
